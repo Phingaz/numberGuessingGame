90144bdda8c824f94496731af214a3486159ab80
Phingaz <piinoya@gmail.com>
2023-01-30 13:35:32 -0300

    bugs and fixes

--- GuessMyNumber/script.js
+++ GuessMyNumber/script.js
@@ -1,18 +1,18 @@
 'use strict'
 
-let score = 20
-let secretNumber = Math.trunc((Math.random() * 20) + 1)
+let score = 15
+let secretNumber = Math.trunc((Math.random() * 30) + 1)
 let highscore = 0
 
-const displayMsg = function(message) {
-     return document.querySelector(".message").textContent = message
+const displayMsg = function (message) {
+    return document.querySelector(".message").textContent = message
 }
 
 document.querySelector(".check").addEventListener("click", (e) => {
     console.log(secretNumber);
-    const guess = Number( document.querySelector(".guess").value )
+    const guess = Number(document.querySelector(".guess").value)
     //If no inout
-    if(!guess) {
+    if (!guess) {
         displayMsg("Enter a number!!")
     }
     //If player wins
@@ -21,14 +21,14 @@ document.querySelector(".check").addEventListener("click", (e) => {
         document.querySelector(".number").textContent = secretNumber
         document.querySelector("body").style.background = "#60b347"
         document.querySelector(".number").style.width = "#30rem"
-        
-        if(score > highscore) {
+
+        if (score > highscore) {
             highscore = score;
-            document.querySelector(".highscore").textContent = highscore 
+            document.querySelector(".highscore").textContent = highscore
         }
     }
     //If player loses
-    else if(guess !== secretNumber) {
+    else if (guess !== secretNumber) {
         if (score > 0) {
             guess > secretNumber ? displayMsg("Too high") : displayMsg("Too low")
             score--
@@ -44,13 +44,13 @@ document.querySelector(".check").addEventListener("click", (e) => {
 })
 
 document.querySelector(".again").addEventListener("click", () => {
-    score = 20
-    secretNumber = Math.trunc((Math.random() * 20) + 1)
+    score = 15
+    secretNumber = Math.trunc((Math.random() * 30) + 1)
     document.querySelector("body").style.background = ""
     displayMsg("Start Guessing...")
-    document.querySelector(".score").textContent = score        
-    document.querySelector(".number").textContent = "?"        
-    document.querySelector(".guess").value = ""        
+    document.querySelector(".score").textContent = score
+    document.querySelector(".number").textContent = "?"
+    document.querySelector(".guess").value = ""
 })
 
 
